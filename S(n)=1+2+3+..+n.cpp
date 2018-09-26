0b7111cb3ddd7e980698517e04bb02138c105061
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-09-26 23:24:58 +0700

    Update S(n)=1+2+3+..+n.cpp

--- S(n)=1+2+3+..+n.cpp
+++ S(n)=1+2+3+..+n.cpp
@@ -1,27 +1,23 @@
 #include<stdio.h>
-//dung care thang (stdlib.h)
+// dung quan tam thang nay
 #include<stdlib.h>
  int main()
  {
-  //khai bao cua ham stdlib.h
-  system("color 02");
-  //tinh tong so nguyen nen khai bao bang integer
-  int n, S=0, i=1; 
-  printf("Nhap n:\n");
-  scanf("%d",&n);
-   if (n <0)
+    system("color 02");
+    int n, S=0, i=1; 
+    printf("Nhap n:\t");
+    scanf("%d",&n);
+ // xet dieu kien n > 0
+    if (n <0)
    		{
 		printf("ban nhap sai");
 		return 0;
 		}
-
-/*code While:
-  B1: May thuc hien cau lenh 1
-  B2: Sau do tinh gia tri bieu thuc 1 ,neu gia tri bthuc sai thi program out. 
-  Neu gia tri bthuc dung thi quay lai B1.Chu y:lenh While ktra dkien truoc moi Run*/		
 	 	while (i<=n)
 		{
 		S+=i;
+// dua ra list cac buoc tinh
+		printf("%d\n",S);
 		i++;	
 		}	
  printf("S(n)= 1 + 2 + 3 +...+ n : %d\n",S);
