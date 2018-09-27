22d6ef733b981119f360e2fb24aceabca0fc88a0
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-09-27 21:23:22 +0700

    Update 1\2+1\4+...+1\2n.cpp

--- 1\2+1\4+...+1\2n.cpp
+++ 1\2+1\4+...+1\2n.cpp
@@ -1,25 +1,30 @@
 #include<stdio.h>
+// dung quan tam thang nay
 #include<stdlib.h>
 int main()
 {
+	//dung quan tam thang nay
 	system("color 04");
 	int i, n;
-	float S =0;
+	// so thap phan nen ta phai khai nao S bang float
+	float S=0;
 	do
 	{
 		printf("\nNhap n: ");
 		scanf("%d", &n);
+		//xet dieu kien nhap vao
 		if(n < 1)
 		{
 			printf("\nN phai lon hon hoac bang 1. nhap lai di!");
 		}
 
 	}
-	while(n < 1);// dung while(n<1) de tao dieu kien nau nhap n<1 thi quay lai nhap dung 
-
+	// dung while(n<1) de tao dieu kien nau nhap n<1 thi quay lai nhap dung 
+ 	while(n < 1);
 	for(i=1;i<=n;i++)
 	{
-		S+= 1.0 / (2*i);  // phai la 1.0 / i, vi S duoc khai bao theo float-point
+		// phai la 1.0 / i, vi S duoc khai bao theo float-point
+		S+= 1.0 / (2*i);
 	}
 	printf("\n 1/2 + 1/4 +...+1/2*%d: %f",n,S);
 	return 0;
