59ec147e29aa566cf055becdb1f410b859c0c8d0
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-09-29 20:32:00 +0700

    Update S-n-1\3+1\5+..1\(2n+1).cpp

--- S-n-1\3+1\5+..1\(2n+1).cpp
+++ S-n-1\3+1\5+..1\(2n+1).cpp
@@ -5,22 +5,25 @@ int main()
     system("color 02 ");
     int a = 1, n;
     float S = 0;
-    	do
+    		do
 		{
 			printf("\nNhap n:");
 			scanf("%d",&n);
 			if(n < 1)
 			{
-				printf("Ban da nhap sai:Moi ban nhap lai:");
+				printf("Nhap sai\nNhap lai:");
 			}
 		}
 		while(n<1);
-			while (a<=n)
+			//while (a<=n)
+			for (a=1;a<=n;a++)
 			{
-			S+=1.0/(2*n +1);
-			printf("%f\n",S);
+			S+=1.0/((2*n) +1);
+		//%f.2 lam tron gia tri float-point den 2
+			printf("list plus%f.2\n",S);
 			a++;
 			}
-		printf("Sn=%f",S);
+	//\t khoang trang
+		printf("Sn=1+1\3+1\5+...+1\(2n+1):\t%f",S);
 		return 0 ;
 }
