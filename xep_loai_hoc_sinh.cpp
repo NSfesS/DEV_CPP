04a1d7853e08e473def0bac93ed3e4dd1aad26a9
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-09-30 18:54:19 +0700

    Update xep_loai_hoc_sinh.cpp

--- xep_loai_hoc_sinh.cpp
+++ xep_loai_hoc_sinh.cpp
@@ -7,13 +7,15 @@ int main()
         printf("\n*------------------ PHONG PHAM ----------------*");
 	float diem;
         printf("Ploai hoc sinh theo diem so, Moi ban nhap diem hoc sinh \n");
-        scanf("%f",&diem);
+       do
+       {
+	     printf("Nhap diem:");
+	     scanf("%f",&diem);
    	 	if (diem < 0 || diem > 10)
-   		
 	    {
 		printf("nhap diem sai ");
-		return 0;
      	}
+       }
      		if (diem > 8 && diem < 10) printf("Loai Gioi");
 		 		else if ( diem >= 6.5 && diem< 8) 
 				printf("Loai Kha ");
