3a7ae032bff51dfddb420a7019f7b8aedada6c1f
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-02 18:51:01 +0700

    Update Bai 3.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai 3.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai 3.cpp
@@ -1,33 +1,32 @@
 #include<stdio.h>
-#include<stdlib.h>
 int main()
 {
-	system("color 04");
-	int i, n;
+	int a, n;
+	a=1;
 	float S;
-	S = 0; i = 1;
+	S = 0;
 	do
 	{
 		printf("\nNhap n: ");
 		scanf("%d", &n);
 		if(n < 1)
 		{
-			printf("\nN phai lon hon hoac bang 1. nhap lai di!");
+			printf("\nPhai nhap n>1. nhap lai di!");
 		}
 
 	}
-	// dung while(n<1) de tao dieu kien neu nhap n<1 thi nhap lai
-	while(n < 1); 
+	// dung while(n<1) de tao dieu kien neu nhap n<1 thi nhap lai.
+	while(n<1); 
 
-	while(i <= n)
+	while(a<=n)
 	{
-		// phai la 1.0 / i, vi S duoc khai bao theo float-point
-		S = S + 1.0 / i; 
-		i++;
+		// phai la 1.0 / i, vi S duoc khai bao theo float-point.
+		S+=1.0/a; 
+		printf("\nS = %f",S);
+		a++;
 	}
-	printf("i = %d", i);
-	printf("\nS = %f", S);
-	printf("\nTong 1 + 1/2 + ... + 1/%d la %.2f: ",n, S);
+	//%.2f la lam tron so thap phan so 2.
+	printf("\nSn= %.2f: ",S);
 	return 0;
 }
-// bai nay co nhieu cach viet khac nhau t co the viet theo cach while cung duoc
+// bai nay co nhieu cach viet khac nhau t co the viet theo cach while cung duoc.
