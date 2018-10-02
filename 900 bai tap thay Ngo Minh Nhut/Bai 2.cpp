fb8fb6f3895d29779430451d956b6116808c25ef
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-02 18:44:41 +0700

    Update Bai 2.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai 2.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai 2.cpp
@@ -1,19 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 int main()
 {
-    system("color 02 ");
-    int i=1,S=0,n;
-    printf("Nhap n bat ky:\t");
-    scanf("%d",&n);
-    	while(i<=n)
+    int a=1,S=0,n;
+    do
+	{
+		printf("Nhap n bat ky:\t");
+        scanf("%d",&n);
+    	if(n<0) printf("Phai nhap n<0, Nhap lai:\t");
+    }
+    while(n<0);
+    	while(a<=n)
     	{
-    		S+=i*i;
-    		// dua ra list cac buoc tinh
+    		S+=a*a;
     		printf("%d\n",S);
-    		i++;
+    		a++;
 		}
 	printf("S = %d\n",S);
+	system("color 05");
 	return 0 ;
 }
-
