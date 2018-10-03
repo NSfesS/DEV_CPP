9fba44426050873bfc24b2dc6c9457eb18a37b31
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-03 12:53:44 +0700

    Update Bai 1.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai 1.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai 1.cpp
@@ -1,21 +1,31 @@
 #include<stdio.h>
- int main()
+int main()
  {
-    int n, S=0, a=1; 
-    printf("\nNhap n:\t");
-    scanf("%d",&n);
- // xet dieu kien n > 0
-	 do{
-    		if (n <0) printf("ban nhap sai. Nhap Lai:");
-	   }
-	 while(n<0);
-	while (a<=n)
-	{
-	S+=a;
-// dua ra list cac buoc tinh
-	printf("%d\n",S);
-	a++;	
-	}	
- printf("S(n)= 1 + 2 + 3 +...+ n : %d\n",S);
- return 0 ;
+    int n, S, a; 
+  	printf("Tinh tong 1+2+3+...+n");
+	do
+		{
+	  	   	printf("\nNhap n:");
+	  	   	scanf("\n%d",&n);
+	  		if (n < 0) printf("Nhap n>=1. Nhap lai!");
+		}
+		while (n < 0);
+ 		/*if (n <0)
+   		{
+		printf("ban nhap sai");
+		return 0;
+		}*/
+		//a=1 vi tinh tong tu 1->n
+		a = 1;
+		S = 0;
+		while (a <= n)
+		{
+			S+= a;
+			printf("S(%d)=%d\n",a,S);
+			a++;	
+		}	
+    printf("\n\t=>S(n)= %d\n",S);
+    return 0 ;
   }
+// bai nay co nhieu cach viet khac nhau ta co the viet theo cach (while) cung duoc^^.
+// chu code khoang trang ro rang, thut ra thut vao hop ly nhe^^.
