4da708d8c9774387f34693927d1d1258a95d67e1
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-03 13:18:41 +0700

    Update Bai 2.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai 2.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai 2.cpp
@@ -21,6 +21,5 @@ int main()
 	printf("\n\t=>Sn= %d",S);
 	return 0 ;
 }
-// bai nay co nhieu cach viet khac nhau ta co the viet theo cach (while) cung duoc^^.
 // chu code khoang trang ro rang, thut ra thut vao hop ly nhe^^.
-
+/**********CHUC CAC BAN HOC TOT********************/
