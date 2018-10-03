e9b7f15cf9bb420626991595fc95126960150353
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-03 13:33:56 +0700

    Update Bai 8.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai 8.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai 8.cpp
@@ -1,25 +1,25 @@
-//DEV CPP WITH PHONG PHAM/https://www.facebook.com/phongpham147
 #include <stdio.h>
 int main()
 {
     int a, n ;
-    	a=1;
     float S;
-    	S=0;
-    	do
-    		{
-    			printf("\nNhap n:\t");
-    			scanf("%d",&n);
-    				if(n<1) printf("\nNhap n>=1. Nhap lai!");
-			}while(n<1);
-				while(a<=n)
-				//for(a=0;a<=n;a++)
-					{
-						S+=float(2*a+1)/(2*a+2);// tren tu la mot complex 
-						printf("%f\n",S);
-						a++;
-					}
-					printf("Sn=...+(2n+1)/(2n+2)=%f",S);
+    printf("Tinh tong 1/2 + 3/4 + 5/6 + ... + 2n + 1/ 2n + 2");
+    do
+    {
+     	printf("\nNhap n:\t");
+    	scanf("%d",&n);
+    	if(n < 1) printf("\nNhap n>=1. Nhap lai!");
+	}
+	while(n < 1);
+	a = 1;
+	S = 0;
+	while(a <= n)
+	{
+		//can ep kieu du lieu (float) de chuyen cong thuc tu gia tri (int) ve (float)
+		S+= float(2*a+1)/(2*a+2);
+		printf("S(%d)= %.3f\n",a,S);
+		a++;
+	}
+	printf("\n\tSn= %.3f",S);
 	return 0 ;
 }
-
