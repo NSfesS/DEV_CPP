241b5ca88cf523871010d5b6a996e75448a511fd
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-03 13:17:36 +0700

    Update Bai 5.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai 5.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai 5.cpp
@@ -3,27 +3,25 @@
 int main()
 {
     system("color 02 ");
-    int a = 1, n;
-    float S = 0;
-    		do
+    int a,n;
+	float S;
+	printf("Tinh tong 1 + 1/3 + 1/5 + ... + 1/(2n + 1)");
+		do	
 		{
 			printf("\nNhap n:");
 			scanf("%d",&n);
-			if(n < 1)
-			{
-				printf("Nhap sai\nNhap lai:");
-			}
+			if(n<0) printf("Nhap sai. Nhap lai");
 		}
-		while(n<1);
-			//while (a<=n)
-			for (a=1;a<=n;a++)
-			{
-			S+=1.0/((2*n) +1);
-		//%f.2 lam tron gia tri float-point den 2
-			printf("list plus%f.2\n",S);
+		while(n<0);
+		a=1;
+		S=0;
+		while (a<=n)
+		{
+			S+=1.0/(a*(a+1));
+			printf("S(%d)=%.3f\n",a,S);
 			a++;
-			}
-	//\t khoang trang
-		printf("Sn=1+1\3+1\5+...+1\(2n+1):\t%f",S);
-		return 0 ;
+		}
+	printf("\n\tSn= %.3f",S);
+	return 0 ;
 }
+// chu code khoang trang ro rang, thut ra thut vao hop ly nhe^^.
