8e84bb77ee8f983de426f86f4890072ac8518125
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-03 12:59:30 +0700

    Update Bai 2.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai 2.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai 2.cpp
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
-    int a=1,n;
-    float S=0;
+    system("color 02 ");
+    int a, n, S;
     do
 	{
-		printf("Nhap n bat ky:\t");
+		printf("\nNhap n:\t");
         scanf("%d",&n);
-    	if(n<0) printf("Phai nhap n>0, Nhap lai:\t");
+    	if(n < 0) printf("Phai nhap n>=0, Nhap lai!");
     }
-    while(n<0);
-    	while(a<=n)
-    	{
-    		S+=a*a;
-    		printf("%f\n",S);
-    		a++;
-		}
-	printf("Tong Sn= %f\n",S);
+    while(n < 0);
+    a=1;
+    S=0;
+    while(a <= n)
+    {
+    	S+= a*a;
+    	printf("S(%d)= %d\n",a,S);
+    	a++;
+	}
+	printf("\n\t=>Sn= %d",S);
+	system("color 05");
 	return 0 ;
 }
+// bai nay co nhieu cach viet khac nhau ta co the viet theo cach (while) cung duoc^^.
+// chu code khoang trang ro rang, thut ra thut vao hop ly nhe^^.
+
