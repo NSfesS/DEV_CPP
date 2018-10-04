69eab795bef94885b8208b33f5ddc1f5b6163663
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-04 18:53:43 +0700

    Update 3.cpp

--- 900 bai tap thay Ngo Minh Nhut/3.cpp
+++ 900 bai tap thay Ngo Minh Nhut/3.cpp
@@ -1,26 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-	int a, n;
+	system("color 02");// mau xanh la 
+	int n, a;
 	float S;
-	printf("Tinh tong 1/2 + 1/4 + ... + 1/2n");
+	printf("Tinh tong 1 + 1/2 + 1/3 + ... + 1/n");
 	do
 	{
 		printf("\nNhap n: ");
 		scanf("%d", &n);
-		if(n < 1) printf("\nNhap n>=1. nhap lai!");
-
+		if(n < 1) printf("\nNhap n>=1. Nhap lai!");
 	}
+	// dung while(n<1) de tao dieu kien neu nhap (n<1) thi nhap lai
 	while(n < 1); 
-	S = 0;
-	a = 1;
+	//vi (1/n) ta co a!=0
+	a=1;
+	S=0;
 	while(a <= n)
 	{
-		//ep (float) de chuyen phep tinh (int) sang (float)
-		S+= float(1) / (2*a);
-		printf("S(%d)= %.3f\n",a,S);
+		// ep (float) vao phep tinh chuyen tu (int) sang (float)
+		printf("S(%d) = %.3f\n",a,S+=float(1) / a);
 		a++;
 	}
-	printf("\n\tSn= %.3f",S);
+	printf("\n\t=>Sn= %.3f: ",S);
+	system("color 05");// mau tim 
 	return 0;
 }
+// bai nay co nhieu cach viet khac nhau ta co the viet theo cach (while) cung duoc^^.
+// chu code khoang trang ro rang, thut ra thut vao hop ly nhe^^.
