39d907d4ec1d0ec393aacf93ab84119d2d67883b
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-04 18:51:13 +0700

    Update 3.cpp

--- 900 bai tap thay Ngo Minh Nhut/3.cpp
+++ 900 bai tap thay Ngo Minh Nhut/3.cpp
@@ -1,29 +1,26 @@
 #include<stdio.h>
 int main()
 {
-	int n,a;
+	int a, n;
 	float S;
-	printf("Tinh tong 1 + 1/2 + 1/3 + ... + 1/n");
+	printf("Tinh tong 1/2 + 1/4 + ... + 1/2n");
 	do
 	{
 		printf("\nNhap n: ");
 		scanf("%d", &n);
-		if(n < 1) printf("\nNhap n>=1. Nhap lai!");
+		if(n < 1) printf("\nNhap n>=1. nhap lai!");
+
 	}
-	// dung while(n<1) de tao dieu kien neu nhap (n<1) thi nhap lai.
 	while(n < 1); 
-	//(1/n) ta co a!=0.
-	a=1;
-	S=0;
+	S = 0;
+	a = 1;
 	while(a <= n)
-	{	
-		// phai la (1.0 / i), vi S duoc khai bao theo (float-point).
-		//%.3f la lam tron den so thap phan thu 3.
-		printf("S(%d) = %.3f\n",a,S+=1.0 / a);
+	{
+		//ep (float) de chuyen phep tinh (int) sang (float)
+		S+= float(1) / (2*a);
+		printf("S(%d)= %.3f\n",a,S);
 		a++;
 	}
-	printf("\n\t=>Sn= %.3f: ",S);
+	printf("\n\tSn= %.3f",S);
 	return 0;
 }
-// chu code khoang trang ro rang, thut ra thut vao hop ly nhe^^.
-/**********CHUC CAC BAN HOC TOT********************/
