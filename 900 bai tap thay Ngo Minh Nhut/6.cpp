838130c6a0d1a51b3754ddfa09c7f14f616d96cb
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-04 18:56:50 +0700

    Update 6.cpp

--- 900 bai tap thay Ngo Minh Nhut/6.cpp
+++ 900 bai tap thay Ngo Minh Nhut/6.cpp
@@ -2,24 +2,25 @@
 #include <stdlib.h>
 int main()
 {
-	system("color 02 ");
-	int a, n;
+    system("color 02 ");
+    int a,n;
 	float S;
-	printf("Tinh tong 1/2 + 2/3 + 3/4 + .... + n / n + 1");
-	do
-	{
-	 	printf("\nNhap n:");
-	 	scanf("%d",&n);
-	 	if(n < 1) printf("Nhap n>=1. Nhap lai");
-	}
-		while (n < 1);
-		a = 1;
-		S = 0;
-		while(a <= n)
+	printf("Tinh tong 1 + 1/3 + 1/5 + ... + 1/n*(n + 1)");
+		do	
 		{
-			printf("S(%d)= %.3f\n",a,S+=a/(a+1));
+			printf("\nNhap n:");
+			scanf("%d",&n);
+			if(n<0) printf("Nhap sai. Nhap lai");
+		}
+		while(n<0);
+		a=1;
+		S=0;
+		while (a<=n)
+		{
+			S+=float(1)/(a*(a+1));
+			printf("S(%d)=%.3f\n",a,S);
 			a++;
 		}
-	printf("\n\t=>Sn= %.3f",S);
+	printf("\n\tSn= %.3f",S);
 	return 0 ;
 }
