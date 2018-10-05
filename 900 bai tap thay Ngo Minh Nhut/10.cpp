0285d5b911ac91bac5df07caec0fd6a634b2a342
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-05 07:53:21 +0700

    Update 10.cpp

--- 900 bai tap thay Ngo Minh Nhut/10.cpp
+++ 900 bai tap thay Ngo Minh Nhut/10.cpp
@@ -10,10 +10,10 @@ int main()
 	
 	printf("\nnhap n=");
 	scanf("%d",&n);
-	
-    T=pow(x,n);
+	// tinh luy thua ta khai bao Phep tinh = pow(hang so , so mu);
+    	T=pow(x,n);
 
-printf("\t Tinh %d^%d=%d",x,n,T);
+	printf("\t Tinh %d^%d=%d",x,n,T);
 
 return 0;
 }
