e1861867d680bb2b9fc9aebf175e0f89fb5e046c
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-13 12:25:42 +0700

    Update 9.cpp

--- 900 bai tap thay Ngo Minh Nhut/9.cpp
+++ 900 bai tap thay Ngo Minh Nhut/9.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int n,a,p,S;
-    printf("Tinh tong 1 x 2 x 3...x N;");
+    int n;
+    printf("Tinh tong 1 x 2 x 3...x N:");
     do
     {
     	printf("\nNhap n:\t");
@@ -10,17 +10,15 @@ int main()
     	if(n<1) printf("Phai nhap n>=1. Nhap lai!:");
 	}
 	while(n<1);
-	a=0;
-	p=1;
+	a=1;
 	S=1;
 	// a<n vì S duoc khai bao (S=1) nen nó duoc tang tu 1->7 
 	//VD nhu (while) la S=[1+(1*1)]*[1+(1*2)]*[1+(1*3)]*...*
-	while(a<n)
+	while(a<=n)
 	{
-		S+=S*a;
-		printf("S(%d)= %d\n",p,S);
+		S*=a;
+		printf("S(%d)= %d\n",a,S);
 		a++;
-		p++;//tac dung la in ra stt S(1)->S(7)
 	}
 	printf("\n\tSn= %d",S);
 	return 0 ;
