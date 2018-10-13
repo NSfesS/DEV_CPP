d2e2f69ea8bb4c42a15cddff778bfa1cc5a508f1
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-13 10:57:16 +0700

    Create bai11.cpp

---
+++ 900 bai tap thay Ngo Minh Nhat/bai11.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int n,flag;
+    flag=1;
+    printf("Tinh S(n) = 1 + 1.2 + 1.2.3 + ... + 1.2.3....N");
+    do
+	{
+		do
+	    {
+	    	printf("\nNhap n:");
+	    	scanf("%d",&n);
+	    	if(n < 0) printf("\nPhai nhap n>0,Nhap lai!:");
+		}while(n<0);
+		int a , S, P;
+		a=1;
+		S=0;
+		P=1;
+		while(a<=n)
+		{
+			P=P*a;
+			S=S+P;
+			printf("\n\tS(%d)=%d",a,S);
+			a++;
+		}
+	printf("\nNhap o de thoat, Nhap so bat ky de NEXT");
+	scanf("%d",&flag);
+	}while(flag!=0);
+	return 0 ;
+}
