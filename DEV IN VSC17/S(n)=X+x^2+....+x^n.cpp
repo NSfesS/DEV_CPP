0806f5c9e33d1fd1b4f892ba0ce1e251b4e97eb5
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-16 17:13:53 +0700

    Create S(n)=X+x^2+....+x^n.cpp

---
+++ DEV IN VSC17/S(n)=X+x^2+....+x^n.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int flag = 1;
+	do
+	{
+		printf("Tinh S(n)=x+x^2+...+x^n");
+		int x;
+		do
+		{
+			printf("\nNhap x:\a");
+			scanf("%d", &x);
+			if (x < 0) printf("\nPhai nhap n > 0,Nhap lai!");
+		} while (x < 0);
+		int n, S = 0;
+		for (n = 1; n <= x; n++)
+		{
+			S = pow(x, n);
+			printf("\nS(%d)=%d", n, S);
+		}
+		printf("\n\tS(n)=%d", S);
+		printf("\nNhap 0 de thoat, Nhap ky tu bat ky de tiep tuc[_]\b\b");
+		scanf("%d", &flag);
+	}while (flag != 0);
+return 0;
+}
