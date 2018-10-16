2d816b4df84d94744cbf5a52991a0c7cc1dfaf62
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-16 17:15:55 +0700

    Update bai12.cpp

--- 900 bai tap thay Ngo Minh Nhut/bai12.cpp
+++ 900 bai tap thay Ngo Minh Nhut/bai12.cpp
@@ -2,26 +2,26 @@
 #include <math.h>
 int main()
 {
-	int flag=1;
+	int flag = 1;
 	do
 	{
 		printf("Tinh S(n)=x+x^2+...+x^n");
 		int x;
 		do
 		{
-			printf("\nNhap x:\a");
+			printf("\nNhap x:\a");// \a phat ra tieng bip khi compiler ^^
 			scanf("%d", &x);
 			if (x < 0) printf("\nPhai nhap n > 0,Nhap lai!");
 		} while (x < 0);
-		int n,S=0;
+		int n, S = 0;
 		for (n = 1; n <= x; n++)
 		{
-			S += pow(x, n);
+			S = pow(x, n);
 			printf("\nS(%d)=%d", n, S);
 		}
-	printf("\nKet qua\tS(n)=%d", S);
-	printf("\nNhap 0 de thoat, Nhap ky tu bat ky de tiep tuc[_]\b\b");
-	scanf("%d",&flag);
-	}while (flag!=0);
-	return 0;
+		printf("\n\tS(n)=%d", S);
+		printf("\nNhap 0 de thoat, Nhap ky tu bat ky de tiep tuc[_]\b\b");
+		scanf("%d", &flag);
+	}while (flag != 0);
+return 0;
 }
