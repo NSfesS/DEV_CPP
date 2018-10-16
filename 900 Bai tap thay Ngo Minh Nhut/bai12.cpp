ec32c0eacd79fd2a13a1e8cde10d5f343efcb654
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-16 11:34:32 +0700

    Create bai12.cpp

---
+++ 900 Bai tap thay Ngo Minh Nhut/bai12.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int flag=1;
+	do
+	{
+		printf("Tinh S(n)=x+x^2+...+x^n");
+		int x;
+		do
+		{
+			printf("\nNhap x");
+			scanf("%d", &x);
+			if (x < 0) printf("\nPhai nhap n > 0,Nhap lai!");
+		} while (x < 0);
+		int n,S=0;
+		for (n = 1; n <= x; n++)
+		{
+			S += pow(x, n);
+			printf("\nS(%d)=%d", n, S);
+		}
+	printf("\nKet qua\tS(n)=%d", S);
+	printf("\nNhap 0 de thoat, Nhap ky tu bat ky de tiep tuc");
+	scanf("%d",&flag);
+	}while (flag!=0);
+	return 0;
+}
