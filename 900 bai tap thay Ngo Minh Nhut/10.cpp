22911df1874cb969472c7d033873cdfd8d3f1daa
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-16 11:46:45 +0700

    Update 10.cpp

--- 900 bai tap thay Ngo Minh Nhut/10.cpp
+++ 900 bai tap thay Ngo Minh Nhut/10.cpp
@@ -5,10 +5,10 @@ int main()
 	printf("Tinh T(x,n)=x^n");
 	int x,n,T; 
 	
-	printf("\nNhap x= ");
+	printf("\nNhap x=\a");// \a phat ra  tieng bip de thong bao user nhap gtri.
 	scanf("%d",&x);
 	
-	printf("\nnhap n=");
+	printf("\nnhap n=\a");
 	scanf("%d",&n);
 	// tinh luy thua ta khai bao Phep tinh = pow(hang so , so mu);
     	T=pow(x,n);
