47247ff2506590610906807efd3943c0350f0705
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-17 16:57:23 +0700

    Create S(n)=X+X^3+...+x^(2n+1).cpp

---
+++ DEV IN VSC17/S(n)=X+X^3+...+x^(2n+1).cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+	system("color 02");
+	int flag = 1;
+	do
+	{
+		float x;
+		printf("S(n) = x + x^3 + x^5 + ... + x^2n + 1");
+		printf("\nNhap x bat ky :\a");
+		scanf("%f", &x);
+		int n;
+		do
+		{
+			printf("\nNhap n:\a");
+			scanf("%d", &n);
+			if (n < 0) printf("\nPhai nhap n > 0,Nhap lai!");
+		} while (n < 0);
+		float S = 0;
+		int i;
+		for (i = 1; i <= n; i++)
+		{
+			S += pow(double(x), double((2 * i) + 1));
+			printf("\nS(%d)=%f", i, S);
+		}
+		printf("\n\tSn=%f", S);
+		printf("\nNhap 0 de thoat, Nhap bat ky de tiep tuc:[_]\b\b");
+		scanf("%d", &flag);
+	} while (flag != 0);
+	return 0;
+}
