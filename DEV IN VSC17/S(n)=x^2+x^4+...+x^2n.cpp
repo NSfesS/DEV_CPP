be649ce4e46d467db9b8d9ee07707f3bcf1f5268
Pham.niit.147 <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-17 16:13:27 +0700

    Create S(n)=x^2+x^4+...+x^2n.cpp

---
+++ DEV IN VSC17/S(n)=x^2+x^4+...+x^2n.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+	system("color 02");
+	int flag = 1;
+	do
+	{
+		float x;
+		int n;
+		do
+		{
+			printf("\n\nTinh S(n) = x^2 + x^4 + ... + x^2n");
+			printf("\nNhap x bat ky:\a");
+			scanf("%f", &x);
+			printf("\nNhap n bat ky:\a");
+			scanf("%d", &n);
+			if (x < 0 || n < 2) printf("\nPhai nhap x > 0 and Nhap n >= 2,Nhap lai!");
+		} while (x < 0 || n < 2);
+		int i;
+		float S = 0;
+		for (i = 1; i <= n; i++)
+		{
+			S += pow(double(x),double(2*i));
+			printf("\nS(%d)=%.3f", i, S);
+		}
+		printf("\n\tS(n)=%.3f", S);
+		printf("\nNhap 0 de thoat, Nhap bat ky de tiep tuc:[_]\b\b");
+		scanf("%d", &flag);
+	} while (flag != 0);
+	return 0;
+}
