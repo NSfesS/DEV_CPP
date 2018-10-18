bccb167ecdb3ae27c832d6b0ed689d0faa935e64
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-18 15:44:58 +0700

    Create Bai15.cpp

---
+++ 900 Bai tap thay Ngo Minh Nhut/Bai15.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+	int flag = 1;
+	do
+	{
+		int n;
+		do
+		{
+			printf("Tinh S(n) = 1 + 1/1 + 2 + 1/ 1 + 2 + 3 + ..... + 1/ 1 + 2 + 3 + .... + N");
+			printf("\nNhap n:\a");
+			scanf("%d", &n);
+			if (n < 0)	printf("\nPhai nhap n > 0,Nhap lai!");
+		} while (n < 0);
+		int i,a=0;
+		float S = 0;
+		for (i = 1; i <= n; i++)
+		{
+			S +=float (1) /(i + a);
+			a++;
+			printf("\nS(%d)=%.3f", i, S);
+		}
+		printf("\n\t=>Sn=%.3f", S);
+		printf("\nNhap 0 de thoat, Nhap bat ky de tiep tuc [_]\b\b");
+		scanf("%d", &flag);
+	} while (flag != 0);
+	return 0;
+}
