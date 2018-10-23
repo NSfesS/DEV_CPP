5d9c3da56a413afacca3e4153ab19576c844d7ec
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-23 17:05:48 +0700

    Create BAI 17(function).cpp

---
+++ DEV IN VSC17/BAI 17(function).cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+float x;
+int n;
+void notifications();
+void input_x_n(float,int);
+float tong(float, int);
+//BEGIN
+int main()
+{
+	notifications();
+	input_x_n(&x,&n);
+	float S = tong(&x, &n);
+	printf("\n\t\t===>>>>Sn = %.3f\n", S);
+	system("pause");
+	return 0;
+}
+//In ra thong bao 
+void notifications()
+{
+  system("color 02");
+	printf("\t\t\t\t\tTINH S(n) = x + x^2/2! + x^3/3! + ... + x^n/N!");
+}
+//Input Data
+void input_x_n(float *x, int*n)
+{
+	printf("\n\t**Input x:\a");
+	scanf("%f", &*x);
+	do
+	{
+		printf("\n\t**Input n:\a");
+		scanf("%d", &*n);
+		if (*n <= 0)	printf("\n>>>Phai nhap n>0.Nhap lai!<<<<");
+	} while (*n <= 0);
+}
+//Tinh Toan
+float tong(float *a, int *b)
+{
+	float Z = 0;
+	int T = 1, i;
+	for (i = 1; i <= *b; i++)
+	{
+		T *= i;
+		Z += pow(double(*a), double(float(i) / T));
+	}
+	return Z;
+}
