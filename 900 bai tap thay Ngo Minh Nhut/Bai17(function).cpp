fb7d2dd053e5e4dd7a7527edf4369103b85b2429
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-23 17:10:05 +0700

    Create Bai17(function).cpp

---
+++ 900 bai tap thay Ngo Minh Nhut/Bai17(function).cpp
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <math.h>
+float x;
+int n;
+void notifications();
+void input_x_n(float *x,int *n);
+float tong(float *a, int*n);
+//BEGIN
+int main()
+{
+	notifications();
+	input_x_n(&x,&n);
+	float S = tong(&x, &n);
+	printf("\n\t\t===>>>>Sn = %.3f\n", S);
+	//system("pause");
+	return 0;
+}
+//In ra thong bao 
+void notifications()
+{
+	printf("\t\t\t\t\tTINH S(n) = x + x^2/2! + x^3/3! + ... + x^n/N!");
+}
+//Input Data
+void input_x_n(float *x, int*n)
+{
+	printf("\n\t**Input x:\a");
+	scanf("%f", &*x);
+	do
+	{
+		printf("\n\t**Input n:\a");
+		scanf("%d", &*n);
+		if (*n <= 0)	printf("\n>>>Phai nhap n>0.Nhap lai!<<<<");
+	} while (*n <= 0);
+}
+//Tinh Toan
+float tong(float *a, int *b)
+{
+	float Z = 0;
+	int T = 1, i;
+	for (i = 1; i <= *b; i++)
+	{
+		T *= i;
+		Z += pow(double(*a), double(float(i) / T));
+	}
+	return Z;
+}
