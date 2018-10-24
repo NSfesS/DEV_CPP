11b10b89b1af7f7ad03099deaca088a68ced9d2e
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-24 17:16:29 +0700

    Update Bai17(function).cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai17(function).cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai17(function).cpp
@@ -1,24 +1,31 @@
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
 float x;
 int n;
 void notifications();
 void input_x_n(float *x,int *n);
-float tong(float *a, int*n);
+float tong(float x, int n);
 //BEGIN
 int main()
 {
-	notifications();
-	input_x_n(&x,&n);
-	float S = tong(&x, &n);
-	printf("\n\t\t===>>>>Sn = %.3f\n", S);
-	//system("pause");
+	int flag = 1;
+	do
+	{
+		notifications();
+		input_x_n(&x, &n);
+		float S = tong(x, n);
+		printf("\n\t\t===>>>>Sn = %.3f\n", S);
+		printf("\n Nhap 0 de thoat. Nhap bat ky de tiep tuc [_]\b\b");
+		scanf("%d", &flag);
+	} while (flag != 0);
 	return 0;
 }
 //In ra thong bao 
 void notifications()
 {
+	system("color 03");
 	printf("\t\t\t\t\tTINH S(n) = x + x^2/2! + x^3/3! + ... + x^n/N!");
 }
 //Input Data
@@ -34,14 +41,15 @@ void input_x_n(float *x, int*n)
 	} while (*n <= 0);
 }
 //Tinh Toan
-float tong(float *a, int *b)
+float tong(float x, int n)
 {
 	float Z = 0;
 	int T = 1, i;
-	for (i = 1; i <= *b; i++)
+	for (i = 1; i <= n; i++)
 	{
 		T *= i;
-		Z += pow(double(*a), double(float(i) / T));
+		Z += pow(double(x), double(float(i) / T));
+		printf("\n\t=>> S(%d) = %.2f", i, Z);
 	}
 	return Z;
 }
