f12f67e6fb9afdf166ebea8b15fa7fdd0cf35746
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-28 20:03:32 +0700

    Create Bai 37.cpp

---
+++ 900 bai tap thay Ngo Minh Nhut/Bai 37.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void notifications();
+void input(int *n);
+float perform(int n);
+int main()
+{
+    int flag = 1;
+    do
+    {
+        int n;
+        notifications();
+        input(&n);
+        perform(n);
+
+        printf("\n\t\t\tNhap 0 de thoat. Nhap bat ky de tiep tuc[_]\b\b");
+        scanf("%d", &flag);
+    } while (flag != 0);
+    return 0;
+}
+void notifications()
+{
+    system("color 03");
+    printf("\n\t\tTINH sqrt(n)(n + sqrt(n) - 1(n - 1 +...+ sqrt(3)(3 + sqrt(2)(2))) co (n-1) sqrt");
+}
+void input(int *n)
+{
+    do
+    {
+        printf("\n**Nhap n: \a");
+        scanf("%d", &*n);
+        if (n <= 0) printf("\n>>PHAI NHAP n > 0. NHAP LAI!");
+    } while (*n <= 0);
+}
+float perform(int n)
+{
+    float S = pow(double(2),double((float(1)/2)));
+    for (int i = 3; i <= n; i++)
+    {
+        S = pow(double(i + S), double(float(1) / i));
+    }
+    printf("\n[KET QUA] Sn = %f", S);
+    return S;
+}
