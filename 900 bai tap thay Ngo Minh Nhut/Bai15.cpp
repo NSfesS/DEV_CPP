46aa73e1edcf8ec0dc9c11c4f211ef5f35622896
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-28 22:26:50 +0700

    Update Bai15.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai15.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai15.cpp
@@ -1,29 +1,44 @@
 #include <stdio.h>
+
+void notifications();
+void input(int *n);
+float perform(int n);
 int main()
 {
-	int flag = 1;
-	do
-	{
-		int n;
-		do
-		{
-			printf("Tinh S(n) = 1 + 1/1 + 2 + 1/ 1 + 2 + 3 + ..... + 1/ 1 + 2 + 3 + .... + N");
-			printf("\nNhap n:\a");
-			scanf("%d", &n);
-			if (n < 0)	printf("\nPhai nhap n > 0,Nhap lai!");
-		} while (n < 0);
-		int i,a=0;
-		float S = 0;
-		for (i = 1; i <= n; i++)
-		{
-			// Chu y ep (float) vi co dap so xe ra so thuc.
-			S +=float (1) /(i + a);
-			a++;
-			printf("\nS(%d)=%.3f", i, S);
-		}
-		printf("\n\t=>Sn=%.3f", S);
-		printf("\nNhap 0 de thoat, Nhap bat ky de tiep tuc [_]\b\b");
-		scanf("%d", &flag);
-	} while (flag != 0);
-	return 0;
+    int flag = 1;
+    do
+    {
+        int n;
+        notifications();
+        input(&n);
+        perform(n);
+        printf("\n\t\tNhap 0 de thoat. Nhap bat ky de tiep tuc[_]\b\b");
+        scanf("%d", &flag);
+    } while (flag != 0);
+    return 0;
+}
+void notifications()
+{
+    printf("\n\t\tTINH Sn = 1 + 1/(1+2)+1/(1+2+3+...+n)");
+}
+void input(int *n)
+{
+    do
+    {
+        printf("\n**Nhap n: \a");
+        scanf("%d", &*n);
+        if (*n <= 0) printf("\nPhai nhap n > 0. Nhap lai!");
+    } while (n <= 0);
+}
+float perform(int n)
+{
+    float S = 0;
+    int T = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        T += i;
+        S += float(1) / T;
+    }
+    printf("\n[KET QUA] Sn = %f", S);
+    return S;
 }
