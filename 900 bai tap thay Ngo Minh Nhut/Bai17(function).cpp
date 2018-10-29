2863f24d1cdfedd2aa68b810bee3a94442140919
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-29 22:03:22 +0700

    Update Bai17(function).cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai17(function).cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai17(function).cpp
@@ -2,51 +2,55 @@
 #include <math.h>
 
 void notifications();
-void input_x_n(float *x, int *n);   
-float perform(float x, int n);
-//BEGIN
+void input(float *x, int *n);
+float mu(float x, int n);
+int giaiTHUA(int x);
 int main()
 {
-	int flag = 1;
-	do
-	{
-		float x;
-		int n;
-		notifications();
-		input_x_n(&x, &n);
-        perform(x, n);
-		printf("\n Nhap 0 de thoat. Nhap bat ky de tiep tuc [_]\b\b");
-		scanf("%d", &flag);
-	} while (flag != 0);
-	return 0;
+    int flag = 1;
+    do
+    {
+        float x;
+        int n;
+        notifications();
+        input(&x, &n);
+        //mu(x, n);
+        //giaiTHUA(n,i);
+        float S = 0;
+        for (int i = 1; i <= n; i++)
+            S += mu(x,i) / giaiTHUA(i);
+        printf("\n\t[KET QUA] Sn = %f\n", S);
+        printf("\nNhap 0 de thoat. Nhap bat ky de tiep tuc[_]\b\b");
+        scanf("%d", &flag);
+    } while (flag != 0);
+    return 0;
 }
-//In ra thong bao 
 void notifications()
 {
-	printf("\t\t\t\t\tTINH S(n) = x + x^2/2! + x^3/3! + ... + x^n/N!");
+    printf("\n\t\t\tTINH x + (x^2)/2! + (x^3)/3! + ... + (x^n)/N!");
 }
-//Input Data
-void input_x_n(float *x, int*n)
+void input(float *x, int *n)
 {
-	printf("\n\t**Input x:\a");
-	scanf("%f", &*x);
-	do
-	{
-		printf("\n\t**Input n:\a");
-		scanf("%d", &*n);
-		if (*n <= 0)	printf("\n>>>Phai nhap n>0.Nhap lai!<<<<");
-	} while (*n <= 0);
+    printf("\n**Nhap x: \a");
+    scanf("%f", &*x);
+    do
+    {
+        printf("\n**Nhap n: \a");
+        scanf("%d", &*n);
+        if (*n < 1) printf("\nNhap n > 0. Nhap lai!");
+    } while (*n < 1);
 }
-//Tinh Toan
-float perform(float x, int n)
+float mu(float x, int n)
 {
-	float S = 0;
-	int T = 1;
-	for (int i = 1; i <= n; i++)
-	{
-        T *= i;
-		S += pow(double(x), double(float(i)))/T;
-	}
-    printf("\n\t[KET QUA] Sn = %f", S);
-	return S;
+    float M =0 ;
+    for (int i = 1; i <= x; i++)
+         M = pow(double(x), double(n));
+    return M;
+}
+int giaiTHUA(int x)
+{
+    int gt = 1;
+    for (int i = 1; i <= x; i++)
+        gt *= i;
+    return gt;
 }
