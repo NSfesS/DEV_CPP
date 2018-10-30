a3d45b61d5858588bb1af5f8ac5cd71f9e82307c
PHONG PHAM <37699545+PHAMNIIT147@users.noreply.github.com>
2018-10-30 07:07:58 +0700

    Update Bai 19.cpp

--- 900 bai tap thay Ngo Minh Nhut/Bai 19.cpp
+++ 900 bai tap thay Ngo Minh Nhut/Bai 19.cpp
@@ -2,18 +2,22 @@
 #include <math.h>
 
 void notifications();
-float x; 
-float n;
-void inputDATA(float *x, float *n);
-float tinhTOAN(float x, float n);
+void input(float *x, int *n);
+float mu(float x, int a);
+int giaiTHUA(int j);
 int main()
 {
 	int flag = 1;
 	do
 	{
+        float x;
+        int  n;
 		notifications();
-		inputDATA(&x, &n);
-		printf("\n*******[KET QUA] Sn = %.3f", tinhTOAN(x, n));
+		input(&x, &n);
+        float S = 0;
+        for (int i = 0; i <= n; i++)
+            S += mu(x,i) / giaiTHUA(i);
+		printf("\n\t=>>[KET QUA] Sn = %f", S + 1);
 		printf("\nNhap 0 de thoat. Nhap so bat ky de tiep tuc[_]\b\b");
 		scanf("%d", &flag);
 	} while (flag != 0);
@@ -23,27 +27,29 @@ void notifications()
 {
 	printf("\t\t\t\tTINH S(n) = 1 + x + x^3/3! + x^5/5! + … + x^(2n+1)/(2n+1)!");
 }
-void inputDATA(float *x, float *n)
+void input(float *x, int *n)
 {
-	printf("\nInput x:\a");
+	printf("\n**Input x:\a");
 	scanf("%f", &*x);
-	printf("\nInput n:\a");
-	scanf("%f", &*n);
+    do
+    {
+        printf("\n**Input n:\a");
+        scanf("%d", &*n);
+        if (*n < 1) printf("\nNhap n > 0. Nhap lai!");
+    } while (*n < 1);
 }
-float tinhTOAN(float x, float n)
+float mu(float x, int a)
 {
-	float T= 0;
-	float gt = 1;
-	float S = 0;
-	int j = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		// phan tu cua ham mu
-		T += (2 * j) + 1;
-		j++;
-		// phan mau giai thua
-		gt *= i; 
-		S += 1 + pow(double(x), double(T / gt));
-	}
-	return S;
+    float M = pow(double(x), double((2 * a) + 1));
+    return M;
+}
+int giaiTHUA(int j)
+{
+    int gt = 1;
+    for (int i = 1; i <= j; i++)
+    {
+        int T = 2 * i + 1;
+        gt *=T*(T-1);
+    }
+    return gt;
 }
